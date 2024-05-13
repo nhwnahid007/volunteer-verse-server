a2a654f99f75aef73053d72acda43733f9cc33b9
nhwnahid007 <nhwnahid@gmail.com>
2024-05-14 04:38:23 +0600

    get data from volunteer collection

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 //middleware
 
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-      ],
-      credentials: true,
-}
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
 
-app.use(cors({corsOptions}));
-app.use(express.json())
-
-
+app.use(cors({ corsOptions }));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omy4kgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,29 +23,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const volunteerCollection = client
+      .db("volunteerVerse")
+      .collection("volunteer");
+    const requestCollection = client
+      .db("volunteerVerse")
+      .collection("requests");
+
+    app.get("/volunteers", async (req, res) => {
+      const result =await volunteerCollection.find().toArray()
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('volunteer verse is running')
-})
+app.get("/", (req, res) => {
+  res.send("volunteer verse is running");
+});
 
 app.listen(port, () => {
-  console.log(`Volunteer verse is running on port ${port}`)
-})
+  console.log(`Volunteer verse is running on port ${port}`);
+});
