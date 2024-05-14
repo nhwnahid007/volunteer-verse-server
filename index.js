5c0030b9cc441cd3b521c9061e28288591baef08
nhwnahid007 <nhwnahid@gmail.com>
2024-05-15 03:24:16 +0600

    delete operation

--- index.js
+++ index.js
@@ -58,6 +58,22 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/volunteer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/managemypost/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await volunteerCollection
+        .find({ 
+          organizer_email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
