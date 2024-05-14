6eab930187eb8828608ad860f64b906bdf3d5a5b
nhwnahid007 <nhwnahid@gmail.com>
2024-05-14 19:24:14 +0600

    get volunteer id from mooongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,12 @@ async function run() {
 
     app.get("/volunteers", async (req, res) => {
       const result =await volunteerCollection.find().toArray()
+      res.send(result)
+    });
+    app.get("/volunteers/:id", async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result =await volunteerCollection.findOne(query)
       res.send(result)
     });
 
